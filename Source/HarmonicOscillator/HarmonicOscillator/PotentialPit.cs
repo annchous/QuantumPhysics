11573b4faf933e9c0384db323a4381026ec5866c
Annchous <annchooousss@gmail.com>
2021-06-06 21:33:19 +0300

    feat: potential pit function

---
+++ Source/HarmonicOscillator/HarmonicOscillator/PotentialPit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HarmonicOscillator
+{
+    public class PotentialPit
+    {
+        public Double Width { get; }
+        public Double PotentialEnergy { get; }
+
+        public PotentialPit(Double width, Double potentialEnergy)
+        {
+            Width = width;
+            PotentialEnergy = potentialEnergy;
+        }
+
+        public Double GetValue(Double x) => Math.Abs(x) <= Width ? -1 * PotentialEnergy : 0;
+    }
+}
